86eb2342abd006818141805de4465d102669b150
Madis Nõmme <me@mad.is>
2018-03-08 10:09:09 +0200

    Disable checking matching variables
    Temporary measure to allow importing secondary languages with
    mismatching variables in them. Translators can then fix them.
    
    Better way would be to make this configurable and then display warning
    in the web UI when there's a variable mismatch.

--- app/models/tolk/translation.rb
+++ app/models/tolk/translation.rb
@@ -7,7 +7,9 @@ module Tolk
     serialize :text
     serialize :previous_text
     validate :validate_text_not_nil, :if => proc {|r| r.primary.blank? && !r.explicit_nil && !r.boolean?}
-    validate :check_matching_variables, :if => proc { |tr| tr.primary_translation.present? }
+    # Turning this validator off until to allow importing secondary locales
+    # with differing variables until better solution
+    # validate :check_matching_variables, :if => proc { |tr| tr.primary_translation.present? }
 
     validates_uniqueness_of :phrase_id, :scope => :locale_id
 
